fc07b75f8e6c286f7945c929d6cef8af662d92e9
Guillaume Dumas <deep@introspection.eu>
2020-02-25 15:41:48 +0100

    Init setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "HyPy",
+    version = "0.1",
+    author = "Florence BRUN, Anaël AYROLLLES, Guillaume DUMAS",
+    author_email = "florence.brun@pasteur.fr, anael.ayrollles@pasteur.fr, guillaume.dumas@pasteur.fr",
+    maintainer = "Guillaume DUMAS",
+    maintainer_email = "guillaume.dumas@pasteur.fr",
+    description = "A hyperscanning toolkit in Python.",
+    license = "BSD",
+    keywords = "hyperscanning EEG",
+    url = "https://github.com/GHFC/HyPy",
+    packages=['hypy'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
