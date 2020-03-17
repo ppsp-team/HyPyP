5b7cf491795aa313d164613a8e0bce59e3e1c4ae
Guillaume Dumas <deep@introspection.eu>
2020-03-17 19:19:41 +0100

    HyPyR -> HyPyP

--- setup.py
+++ setup.py
@@ -2,17 +2,21 @@ import os
 from setuptools import setup
 
 setup(
-    name = "HyPyR",
+    name = "HyPyP",
     version = "0.1",
-    author = "Florence BRUN, Anaël AYROLLLES, Guillaume DUMAS",
-    author_email = "florence.brun@pasteur.fr, anael.ayrollles@pasteur.fr, guillaume.dumas@pasteur.fr",
+    author = ("Florence BRUN, Anaël AYROLLLES, ",
+              "Phoebe CHEN, Amir DJALOVSKI, ",
+              "Suzanne DIKKER, Guillaume DUMAS"),
+    author_email = ("florence.brun@pasteur.fr, anael.ayrollles@pasteur.fr,",
+                    "phoebe.chen1117@gmail.com, amir.djv@gmail.com,",
+                    "suzanne.dikker@nyu.edu, guillaume.dumas@pasteur.fr"),
     maintainer = "Guillaume DUMAS",
     maintainer_email = "guillaume.dumas@pasteur.fr",
-    description = "A hyperscanning toolkit in Python.",
+    description = "The Hyperscanning Python Pipeline.",
     license = "BSD",
     keywords = "hyperscanning EEG",
-    url = "https://github.com/GHFC/HyPyR",
-    packages=['hypyr'],
+    url = "https://github.com/GHFC/HyPyP",
+    packages=['hypyp'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Topic :: Scientific/Engineering",
