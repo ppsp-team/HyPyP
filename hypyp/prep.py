c36ad34d3fea33d9fa93d8cab40cae22a680cc00
aayrolles <60617725+aayrolles@users.noreply.github.com>
2020-03-18 17:36:32 +0100

    File description

--- hypyp/prep.py
+++ hypyp/prep.py
@@ -1,1 +1,10 @@
-
+#!/usr/bin/env python
+# coding=utf-8
+# ==============================================================================
+# title           : prep.py
+# description     : data preprocessing functions
+# author          : Guillaume Dumas, Florence Brun, Anaël Ayrolles
+# date            : 2020-03-18
+# version         : 1
+# python_version  : 3.7
+# ==============================================================================
