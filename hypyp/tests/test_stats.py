3715cf0137ed7cb846f8885ec1ed4db24ad2131a
Fbrun18 <florence.brun@ens-lyon.fr>
2020-04-07 13:05:09 +0200

    'modify indexes for data hypyp'

--- hypyp/tests/test_stats.py
+++ hypyp/tests/test_stats.py
@@ -32,25 +32,28 @@ def test_metaconn():
     metaconn, metaconn_freq = stats.metaconn_matrix_2brains(
         sensor_pairs, ch_con, frequencies)
     # take a random ch_name:
-    random.seed(42)  # Init the random number generator for reproducibility
-    n = random.randrange(0, 63)
-    p = random.randrange(63, 125)
+    random.seed(20)  # Init the random number generator for reproducibility
+    # n = random.randrange(0, 63)
+    # for our data taske into account EOG ch!!!
+    n = random.randrange(0, len(epo1.info['ch_names']))
+    tot = len(epoch_merge.info['ch_names'])
+    p = random.randrange(len(epo1.info['ch_names'], tot)
     # checking for each pair in which ch_name is,
     # whether ch_name linked himself
     # (in neighbouring frequencies also)
-    assert(metaconn_freq[n+62, p] == metaconn_freq[n, p])
-    assert(metaconn_freq[n-62, p] == metaconn_freq[n, p])
-    assert(metaconn_freq[n+62, p+62] == metaconn_freq[n, p])
-    assert(metaconn_freq[n-62, p-62] == metaconn_freq[n, p])
-    assert(metaconn_freq[n, p+62] == metaconn_freq[n, p])
-    assert(metaconn_freq[n, p-62] == metaconn_freq[n, p])
+    assert(metaconn_freq[n+tot, p] == metaconn_freq[n, p])
+    assert(metaconn_freq[n-tot, p] == metaconn_freq[n, p])
+    assert(metaconn_freq[n+tot, p+tot] == metaconn_freq[n, p])
+    assert(metaconn_freq[n-tot, p-tot] == metaconn_freq[n, p])
+    assert(metaconn_freq[n, p+tot] == metaconn_freq[n, p])
+    assert(metaconn_freq[n, p-tot] == metaconn_freq[n, p])
     # and not in the other frequencies
     for i in range(1, len(frequencies)):
-        assert(metaconn_freq[n+62*(i+1), p] != metaconn_freq[n, p])
-        assert(metaconn_freq[n-62*(i+1), p] != metaconn_freq[n, p])
-        assert(metaconn_freq[n+62*(i+1), p+62*(i+1)] != metaconn_freq[n, p])
-        assert(metaconn_freq[n-62*(i+1), p-62*(i+1)] != metaconn_freq[n, p])
-        assert(metaconn_freq[n, p+62*(i+1)] != metaconn_freq[n, p])
-        assert(metaconn_freq[n, p-62*(i+1)] != metaconn_freq[n, p])
+        assert(metaconn_freq[n+tot*(i+1), p] != metaconn_freq[n, p])
+        assert(metaconn_freq[n-tot*(i+1), p] != metaconn_freq[n, p])
+        assert(metaconn_freq[n+tot*(i+1), p+62*(i+1)] != metaconn_freq[n, p])
+        assert(metaconn_freq[n-tot*(i+1), p-62*(i+1)] != metaconn_freq[n, p])
+        assert(metaconn_freq[n, p+tot*(i+1)] != metaconn_freq[n, p])
+        assert(metaconn_freq[n, p-tot*(i+1)] != metaconn_freq[n, p])
         # check for each f if connects to the good other ch and not to more
-        assert(metaconn_freq[n+62*i, p+62*i] == ch_con_freq[n, p-62])
+        assert(metaconn_freq[n+tot*i, p+tot*i] == ch_con_freq[n, p-tot])
