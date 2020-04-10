9ce14bfc3657d004cff847a53dccaf3d572a96d0
Fbrun18 <florence.brun@ens-lyon.fr>
2020-04-10 14:32:17 +0200

    change PSD function

--- hypyp/analyses.py
+++ hypyp/analyses.py
@@ -18,71 +18,59 @@ from mne.time_frequency import psd_welch
 from mne.io.constants import FIFF
 
 
-def PSD(epochs_baseline, epochs_task, fmin, fmax):
+def PSD(epochs, fmin, fmax, time_resolved):
     """
     Compute the Power Spectral Density (PSD) on Epochs for a condition and
     normalize by the PSD of the baseline.
 
     Parameters
     -----
-    epochs_baseline, epochs_task : Epochs for the baseline and the condition
-    ('task' for example), for a subject. epochs_baseline, epochs_task result
+    epochs : Epochs for a condition, for a subject (can result
     from the concatenation of epochs from different occurences of the condition
-    across experiments. Epochs are MNE objects (data are stored in arrays of
+    across experiments). Epochs are MNE objects (data are stored in arrays of
     shape (n_epochs, n_channels, n_times) and info are into a dictionnary).
 
     Note that the function can be iterated on the group and/or on conditions:
-    for epochs_baseline, epochs_task in zip(
-        epochs['epochs_%s_%s_%s_baseline' % (subj, group, cond_name)],
-        epochs['epochs_%s_%s_%s_task' % (subj, group, cond_name)]).
+    for epochs in epochs['epochs_%s_%s_%s' % (subj, group, cond_name)], you can
+    then visualize PSD distribution on the group with the toolbox vizualisation
+    to check normality for statistics for example.
 
-    You can then visualize PSD distribution on the group with the toolbox
-    vizualisation to check normality for statistics for example.
+    fmin, fmax : minimum and maximum frequencies-of-interest for power spectral
+    density calculation (in Hz).
 
-    fmin, fmax : minimum and maximum frequencies for PSD (in Hz).
+    time_resolved : boolean
+    Whether to collapse the time course, only effective when set tot True.
+    If False, PSD won't be averaged over epochs, and the time course
+    is maintained. If True, PSD values are averaged over epochs.
 
     Returns
     -----
     freqs_mean : list of frequencies in frequency-band-of-interest used by MNE
     for power spectral density calculation.
 
-    m_baseline, psds_welch_task_m : ndarray
-    PSD average across epochs for each channel and each frequency,
-    for the baseline and the 'task' condition respectively.
+    PSD_welch : ndarray (n_epochs, n_channels, n_frequencies)
+    PSD value in epochs for each channel and each frequency.
+    Note that if time_resolved==True, PSD values are averaged across epochs.
 
-    psd_mean_task_normZ, psd_mean_task_normLog : ndarray
-    Zscore and Logratio of the average PSD during 'task' condition
     """
     # dropping EOG channels (incompatible with connectivity map model in stats)
-    for ch in epochs_baseline.info['chs']:
+    for ch in epochs.info['chs']:
         if ch['kind'] == 202:  # FIFFV_EOG_CH
-            epochs_baseline.drop_channels([ch['ch_name']])
-    for ch in epochs_task.info['chs']:
-        if ch['kind'] == 202:  # FIFFV_EOG_CH
-            epochs_task.drop_channels([ch['ch_name']])
+            epochs.drop_channels([ch['ch_name']])
 
     # computing power spectral density on epochs signal
     # average in the 1second window around event (mean but can choose 'median')
     kwargs = dict(fmin=fmin, fmax=fmax, n_jobs=1)
-    psds_welch_baseline, freqs_mean = psd_welch(
-        epochs_baseline, **kwargs, average='mean', picks='all')  # or median
-    psds_welch_task, freqs_mean = psd_welch(
-        epochs_task, **kwargs, average='mean', picks='all')  # or median
+    psds_welch, freqs_mean = psd_welch(
+        epochs, **kwargs, average='mean', picks='all')  # or median
 
-    # averaging power across epochs for each ch and each f
-    m_baseline = np.mean(psds_welch_baseline, axis=0)
-    std_baseline = np.std(psds_welch_baseline, axis=0)
-    psds_welch_task_m = np.mean(psds_welch_task, axis=0)
+    if time_resolved is True:
+        # averaging power across epochs for each ch and each f
+        PSD_welch = np.mean(psds_welch, axis=0)
+    else:
+        PSD_welch = psds_welch
 
-    # normalizing power during task by baseline average power across events
-    # Z score
-    s = np.subtract(psds_welch_task_m, m_baseline)
-    psd_mean_task_normZ = np.divide(s, std_baseline)
-    # Log ratio
-    d = np.divide(psds_welch_task_m, m_baseline)
-    psd_mean_task_normLog = np.log10(d)
-
-    return freqs_mean, m_baseline, psds_welch_task_m, psd_mean_task_normZ, psd_mean_task_normLog
+    return freqs_mean, PSD_welch
 
 
 def indexes_connectivity_intrabrain(epochs):
