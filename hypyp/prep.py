e293a107b4dd163e8775010fb599b3f6da176ca0
Fbrun18 <florence.brun@ens-lyon.fr>
2020-04-14 15:37:56 +0200

    improved docu and pep8

--- hypyp/prep.py
+++ hypyp/prep.py
@@ -17,28 +17,31 @@ from mne.preprocessing import ICA, corrmap
 
 
 def filt(raw_S):
-    """Before ICA : Filter raw data to remove slow drifts
+    """
+    Filter list of raw data to remove slow drifts.
+
     Parameters
-    ----------
-    raw_S : list of raws for different occurences of a condition, for
-    a subject.
+    -----
+    raw_S : list of Raw data (as an example: different occurences of
+            a condition for a subject). Raws are MNE objects.
 
     Returns
-    -------
+    -----
     raws : list of high-pass filtered raws.
-    
+
     """
     raws = []
-    for raw in raw_S: # per subj
+    for raw in raw_S:
         raws.append(mne.io.Raw.filter(raw, l_freq=2., h_freq=None))
-    
+
     return raws
 
 
 def ICA_choice_comp(icas, epochs):
-    """Plot Independant Components for each subject, let the user
-    choose the relevant components for artefacts rejection and apply ICA
-    on Epochs.
+    """
+    Plot Independant Components for each subject, let the user choose
+    the relevant components for artefacts rejection and apply ICA on
+    Epochs.
 
     Parameters
     -----
@@ -53,7 +56,7 @@ def ICA_choice_comp(icas, epochs):
 
     Returns
     -----
-    List of 2 clean Epochs (for each subject).
+    cleaned_epochs_ICA : list of 2 cleaned Epochs for each subject.
 
     """
     # plotting Independant Components for each subject
@@ -83,26 +86,28 @@ def ICA_choice_comp(icas, epochs):
 
 
 def ICA_apply(icas, subj_number, comp_number, epochs):
-    """Apply ICA with template model from 1 subject in the dyad."""
-    
+    """
+    Apply ICA with template model from 1 subject in the dyad.
+    """
+
     cleaned_epochs_ICA = []
-    ## selecting which ICs corresponding to the template 
+    # selecting which ICs corresponding to the template 
     template_eog_component = icas[subj_number].get_components()[:,comp_number]
 
-    ## applying corrmap with at least 1 component detected for each subj
+    # applying corrmap with at least 1 component detected for each subj
     fig_template, fig_detected = corrmap(icas, template=template_eog_component, threshold=0.9, label='blink', ch_type='eeg')
 
-    ## labeling the ICs that capture blink artifacts
+    # labeling the ICs that capture blink artifacts
     print([ica.labels_ for ica in icas])
 
-    ## selecting ICA components after viz
+    # selecting ICA components after viz
     for i in icas:
         i.exclude = i.labels_['blink']
 
     epoch_all_ch = []
-    ## applying ica on clean_epochs
+    # applying ica on clean_epochs
     # for each subject
-    for i,j in zip(range(0,len(epochs)),icas):
+    for i, j in zip(range(0, len(epochs)), icas):
         # taking all channels to apply ICA
         bads = epochs[i].info['bads']
         epoch_all_ch.append(mne.Epochs.copy(epochs[i]))
@@ -115,7 +120,8 @@ def ICA_apply(icas, subj_number, comp_number, epochs):
 
 
 def ICA_fit(epochs, n_components, method, random_state):
-    """Compute global Autorejection to fit Independant Components Analysis
+    """
+    Compute global Autorejection to fit Independant Components Analysis
     on Epochs, for each subject.
 
     Pre requisite : install autoreject
@@ -132,10 +138,10 @@ def ICA_fit(epochs, n_components, method, random_state):
              sampling parameters).
 
     n_components : the number of principal components that are passed to the
-                   ICA algorithm during fitting, for a first estimation,
+                   ICA algorithm during fitting, int. For a first estimation,
                    n_components can be set to 15.
 
-    method : the ICA method used, 'fastica', 'infomax' or 'picard'.
+    method : the ICA method used, str 'fastica', 'infomax' or 'picard'.
              Fastica' is the most frequently used.
 
     random_state : the parameter used to compute random distributions
@@ -143,45 +149,46 @@ def ICA_fit(epochs, n_components, method, random_state):
                    random_state value to have reproducible results. For 15
                    components, random_state can be set to 97 for example.
 
-    Info
-    -----
-    If Autoreject and ICA take too much time, change the decim value (see MNE
-    documentation).
+    Note that if Autoreject and ICA take too much time, change the decim value
+    (see MNE documentation).
 
     Returns
     -----
-    List of independant components for each subject. IC are MNE objects, see
-    MNE documentation for more details.
+    icas : list of independant components for each subject. IC are MNE objects,
+    see MNE documentation for more details.
     """
     icas = []
-    for epoch in epochs: # per subj
-             
-        ## applying AR to find global rejection threshold
-        reject = get_rejection_threshold(epoch,ch_types='eeg') 
+    for epoch in epochs:
+        # per subj
+        # applying AR to find global rejection threshold
+        reject = get_rejection_threshold(epoch, ch_types='eeg')
         # if very long, can change decim value
         print('The rejection dictionary is %s' % reject)
 
-        ## fitting ICA on filt_raw after AR
+        # fitting ICA on filt_raw after AR
         ica = ICA(n_components=15, method='fastica', random_state=97)
         # take bad channels into account in ICA fit
         epoch_all_ch = mne.Epochs.copy(epoch)
         epoch_all_ch.info['bads'] = []
-        icas.append(ica.fit(epoch_all_ch,reject=reject, tstep=1))
+        icas.append(ica.fit(epoch_all_ch, reject=reject, tstep=1))
 
     return icas
 
 
 def AR_local(cleaned_epochs_ICA, verbose=False):
-    """Apply local Autoreject
+    """
+    Apply local Autoreject to correct or reject bad epochs.
+
     Parameters
-    ----------
-    clean_epochs_ICA : instance of Epochs after global Autoreject
-                         and ICA
-    verbose : Boolean for verbose and plot of data before/after AR 
+    -----
+    clean_epochs_ICA : list of Epochs after global Autoreject and ICA
+
+    verbose : to plot data before and after AR, boolean set to False
+              by default.
 
     Returns
-    -------
-    cleaned_epochs_AR : instance of Epochs after local Autoreject
+    -----
+    cleaned_epochs_AR : list of Epochs after local Autoreject.
     """
     bad_epochs_AR = []
 
