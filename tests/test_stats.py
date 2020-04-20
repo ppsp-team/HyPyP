2a6db65225adab1fb5c14f4893fda2e8f75be506
Fbrun18 <florence.brun@ens-lyon.fr>
2020-04-20 14:29:44 +0200

    corrected for hypyp tuples output + black

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import os
 import random
 import numpy as np
 import scipy
@@ -20,19 +19,23 @@ def test_metaconn_matrix_2brains(epochs):
     # taking random freq-of-interest to test metaconn_freq
     freq = [11, 12, 13]
     # computing ch_con and sensors pairs for metaconn calculation
-    ch_con, ch_con_freq = stats.con_matrix(epochs.epo1, freq, draw=False)
-    sensor_pairs = analyses.indexes_connectivity_interbrains(epochs.epoch_merge)
+    con_matrixTuple = stats.con_matrix(epochs.epo1, freq, draw=False)
+    ch_con_freq = con_matrixTuple.ch_con_freq
+    sensor_pairs = analyses.indexes_connectivity_interbrains(
+        epochs.epoch_merge)
 
     # computing metaconn_freq and test it
-    _, metaconn_freq = stats.metaconn_matrix_2brains(sensor_pairs,
-                                                     ch_con, freq)
+    metaconn_matrix_2brainsTuple = stats.metaconn_matrix_2brains(sensor_pairs,
+                                                                 con_matrixTuple.ch_con, freq)
+    metaconn_freq = metaconn_matrix_2brainsTuple.metaconn_freq
     # take a random ch_name:
     random.seed(20)  # Init the random number generator for reproducibility
     # n = random.randrange(0, 63)
     # for our data taske into account EOG ch!!!
     n = random.randrange(0, len(epochs.epo1.info['ch_names']))
     tot = len(epochs.epo1.info['ch_names'])
-    p = random.randrange(len(epochs.epo1.info['ch_names']), len(epochs.epoch_merge.info['ch_names'])+1)
+    p = random.randrange(len(epochs.epo1.info['ch_names']), len(
+        epochs.epoch_merge.info['ch_names'])+1)
     # checking for each pair in which ch_name is,
     # whether ch_name linked himself
     # (in neighbouring frequencies also)
@@ -47,8 +50,10 @@ def test_metaconn_matrix_2brains(epochs):
         for i in range(1, len(freq)):
             assert metaconn_freq[n+tot*(i+1), p] != metaconn_freq[n, p]
             assert metaconn_freq[n-tot*(i+1), p] != metaconn_freq[n, p]
-            assert metaconn_freq[n+tot*(i+1), p+tot*(i+1)] != metaconn_freq[n, p]
-            assert metaconn_freq[n-tot*(i+1), p-tot*(i+1)] != metaconn_freq[n, p]
+            assert metaconn_freq[n+tot *
+                                 (i+1), p+tot*(i+1)] != metaconn_freq[n, p]
+            assert metaconn_freq[n-tot *
+                                 (i+1), p-tot*(i+1)] != metaconn_freq[n, p]
             assert metaconn_freq[n, p+tot*(i+1)] != metaconn_freq[n, p]
             assert metaconn_freq[n, p-tot*(i+1)] != metaconn_freq[n, p]
             # check for each f if connects to the good other ch and not to more
@@ -87,35 +92,28 @@ def test_simple_corr(epochs):
 
     # trace running time
     now = time.time()
-    # mode to transform signal to analytic signal on which
-    # synchrony is computed
+    # mode to transform signal to analytic signal
+    # on which synchrony is computed
     # mode = 'fourier'
     mode = 'multitaper'
 
     # Phoebe: multitaper: mne.time_frequency.tfr_array_multitaper
     # BUT step = 1s, while coh (including the multitaper step) < 1s...
     # optimized in MNE
-    # how to optimize the mutitaper step in Phoebe script?
-    # and then the second step: same question
 
-    coh_mne,_,_,_,_ = mne.connectivity.spectral_connectivity(data=data_mne,
-                                                             method='plv',
-                                                             mode=mode,
-                                                             indices=sensors,
-                                                             sfreq=500,
-                                                             fmin=11,
-                                                             fmax=13,
-                                                             faverage=True)
+    coh_mne, _, _, _, _ = mne.connectivity.spectral_connectivity(data=data_mne,
+                                                                 method='plv',
+                                                                 mode=mode,
+                                                                 indices=sensors,
+                                                                 sfreq=500,
+                                                                 fmin=11,
+                                                                 fmax=13,
+                                                                 faverage=True)
     now2 = time.time()
-    # coh = analyses.simple_corr(data, frequencies, mode='plv',
-    #                            epoch_wise=True,
-    #                            time_resolved=True)
-    # substeps cf. multitaper step too long?
+    # coh = analyses.simple_corr(data, frequencies, mode='plv')
     values = analyses.compute_single_freq(data, frequencies)
     now3 = time.time()
-    result = analyses.compute_sync(values, mode='plv',
-                                   epoch_wise=True,
-                                   time_resolved=True)
+    result = analyses.compute_sync(values, mode='plv')
     now4 = time.time()
     # convert time to pick seconds only in GTM ref
     now = time.localtime(now)
@@ -130,17 +128,11 @@ def test_simple_corr(epochs):
     # idem en inter-ind
 
     # test substeps
-    assert (int(now2.tm_sec) - int(now.tm_sec)) == ((int(now4.tm_sec) - int(now3.tm_sec))+(int(now3.tm_sec) - int(now2.tm_sec)))
+    assert (int(now2.tm_sec) - int(now.tm_sec)) == ((int(now4.tm_sec) -
+                                                     int(now3.tm_sec))+(int(now3.tm_sec) - int(now2.tm_sec)))
     # each of the two steps in Phoebe script takes 1 second
     # (while first step less than 1s for the MNE function cf. multitaper
     # calculation... = ?)
-
-    # PB: test not stable, time changes!!
-
-    # assess results: shape equivalence and values
-    # not same output: MNE pairs of electrode (n_connections=31*31, freq=1),
-    # Phoebe (31, 31, 1)
-    # assert coh[0][0][0] == coh_mne[0][0]
 
 
 def test_ICA(epochs):
@@ -151,11 +143,11 @@ def test_ICA(epochs):
     icas = prep.ICA_fit(ep, n_components=15, method='fastica', random_state=97)
     # check that the number of componenents is similar between the two subjects
     for i in range(0, len(icas)-1):
-        mne.preprocessing.ICA.get_components(icas[i]).shape == mne.preprocessing.ICA.get_components(icas[i+1]).shape
+        mne.preprocessing.ICA.get_components(
+            icas[i]).shape == mne.preprocessing.ICA.get_components(icas[i+1]).shape
     # check corrmap ok on the 2 subj
     # check component choice good cf. compare with find eog or find ecg comp
-    # cleaned_epochs_ICA = prep.ICA_choice_comp(icas, ep)
-    # can not resolve the interactive window !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # cleaned_epochs_ICA = prep.ICA_choice_comp(icas, ep) # pb interactive window
     # check signal better after than before
     # check bad channels are not deleted
     # assert epochs.epo1.info['ch_names'] == cleaned_epochs_ICA[0].info['ch_names']
@@ -188,15 +180,20 @@ def test_PSD(epochs):
     """
     fmin = 10
     fmax = 13
-    freqs_mean, PSD_welch = analyses.PSD(epochs.epo1,
-                                         fmin, fmax,
-                                         time_resolved=True)
+    PSDTuple = analyses.PSD(epochs.epo1,
+                            fmin, fmax,
+                            time_resolved=True)
+    PSD_welch = PSDTuple.PSD_welch
+    freqs_mean = PSDTuple.freqs_mean
     assert type(PSD_welch) == np.ndarray
-    assert PSD_welch.shape == (len(epochs.epo1.info['ch_names']), len(freqs_mean))
-    freqs_mean, PSD_welch = analyses.PSD(epochs.epo1,
-                                         fmin, fmax,
-                                         time_resolved=False)
-    assert PSD_welch.shape == (len(epochs.epo1), len(epochs.epo1.info['ch_names']), len(freqs_mean))
+    assert PSD_welch.shape == (
+        len(epochs.epo1.info['ch_names']), len(freqs_mean))
+    PSDTuple = analyses.PSD(epochs.epo1,
+                            fmin, fmax,
+                            time_resolved=False)
+    PSD_welch = PSDTuple.PSD_welch
+    assert PSD_welch.shape == (len(epochs.epo1), len(
+        epochs.epo1.info['ch_names']), len(freqs_mean))
 
 
 def test_indexes_connectivity(epochs):
@@ -210,9 +207,9 @@ def test_indexes_connectivity(epochs):
         L.append(length-i)
     tot = sum(L)
     assert len(electrodes) == tot
-    electrodes_hyper = analyses.indexes_connectivity_interbrains(epochs.epoch_merge)
+    electrodes_hyper = analyses.indexes_connectivity_interbrains(
+        epochs.epoch_merge)
     assert len(electrodes_hyper) == length*length
-    # check output type: list of tuples
     # format that do not work for mne.spectral_connectivity #TODO: change that
     # cf. do not needed for Phoebe simple_corr function!
 
@@ -223,9 +220,10 @@ def test_stats(epochs):
     """
     fmin = 10
     fmax = 13
-    freqs_mean, PSD_welch = analyses.PSD(epochs.epo1,
-                                         fmin, fmax,
-                                         time_resolved=False)
+    PSDTuple = analyses.PSD(epochs.epo1,
+                            fmin, fmax,
+                            time_resolved=False)
+    PSD_welch = PSDTuple.PSD_welch
 
     statsCondTuple = stats.statsCond(PSD_welch, epochs.epo1, 3000, 0.05, 0.05)
     assert statsCondTuple.T_obs.shape[0] == len(epochs.epo1.info['ch_names'])
@@ -234,24 +232,32 @@ def test_stats(epochs):
     # and retest with time_resolved=True
     for i in range(0, len(statsCondTuple.p_values)):
         assert statsCondTuple.p_values[i] <= statsCondTuple.adj_p[1][i]
-    assert statsCondTuple.T_obs_plot.shape[0] == len(epochs.epo1.info['ch_names'])
+    assert statsCondTuple.T_obs_plot.shape[0] == len(
+        epochs.epo1.info['ch_names'])
     # test T_obs_plot with viz function
 
-    _, PSD_welch2 = analyses.PSD(epochs.epo2,
-                                 fmin, fmax,
-                                 time_resolved=False)
+    PSDTuple2 = analyses.PSD(epochs.epo2,
+                             fmin, fmax,
+                             time_resolved=False)
+    PSD_welch2 = PSDTuple2.PSD_welch
+    freqs_mean = PSDTuple2.freqs_mean
+
     data = [PSD_welch, PSD_welch2]
     con_matrixTuple = stats.con_matrix(epochs.epo1, freqs_mean, draw=False)
     statscondClusterTuple = stats.statscondCluster(data,
                                                    freqs_mean,
-                                                   scipy.sparse.bsr_matrix(con_matrixTuple.ch_con_freq),
+                                                   scipy.sparse.bsr_matrix(
+                                                       con_matrixTuple.ch_con_freq),
                                                    tail=0,
                                                    n_permutations=3000,
                                                    alpha=0.05)
-    assert statscondClusterTuple.F_obs.shape[0] == len(epochs.epo1.info['ch_names'])
+    assert statscondClusterTuple.F_obs.shape[0] == len(
+        epochs.epo1.info['ch_names'])
     for i in range(0, len(statscondClusterTuple.clusters)):
-        assert len(statscondClusterTuple.clusters[i]) < len(epochs.epo1.info['ch_names'])
-    assert statscondClusterTuple.cluster_p_values.shape[0] == len(statscondClusterTuple.clusters)
+        assert len(statscondClusterTuple.clusters[i]) < len(
+            epochs.epo1.info['ch_names'])
+    assert statscondClusterTuple.cluster_p_values.shape[0] == len(
+        statscondClusterTuple.clusters)
     # test F_obs_plot (ntests,) with viz function
 
 
@@ -262,7 +268,8 @@ def test_utils(epochs):
     ep_hyper = utils.merge(epochs.epo1, epochs.epo2)
     assert type(ep_hyper) == mne.epochs.EpochsArray
     # check channels number
-    assert len(ep_hyper.info['ch_names']) == 2*len(epochs.epo1.info['ch_names'])
+    assert len(ep_hyper.info['ch_names']) == 2 * \
+        len(epochs.epo1.info['ch_names'])
     # check EOG channels number
 
     # check data for S2 or 1 correspond in the ep_hyper, on channel n and
