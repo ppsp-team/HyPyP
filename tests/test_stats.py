ac7cd5117efa9e77a0a761d025de19c01387685e
Fbrun18 <florence.brun@ens-lyon.fr>
2020-04-28 12:13:39 +0200

    comment test shape statcond

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -227,12 +227,12 @@ def test_stats(epochs):
 
     statsCondTuple = stats.statsCond(PSD_welch, epochs.epo1, 3000, 0.05, 0.05)
     assert statsCondTuple.T_obs.shape[0] == len(epochs.epo1.info['ch_names'])
-
-    PSDTuple = analyses.PSD(epochs.epo1,
-                            fmin, fmax,
-                            time_resolved=True)
-    PSD_welch2 = PSDTuple.PSD_welch
-    statsCondTuple = stats.statsCond(PSD_welch2, epochs.epo1, 3000, 0.05, 0.05)
+    # test shape assertion in the function
+    # PSDTuple = analyses.PSD(epochs.epo1,
+    #                         fmin, fmax,
+    #                         time_resolved=True)
+    # PSD_welch2 = PSDTuple.PSD_welch
+    # statsCondTuple = stats.statsCond(PSD_welch2, epochs.epo1, 3000, 0.05, 0.05)
 
     for i in range(0, len(statsCondTuple.p_values)):
         assert statsCondTuple.p_values[i] <= statsCondTuple.adj_p[1][i]
