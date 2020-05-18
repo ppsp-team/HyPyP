cbc703eab9d209bcacdb72c845938421d781aaa0
Guillaume Dumas <deep@introspection.eu>
2020-05-18 10:30:11 +0200

    Update setup.py for pre-alpha

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 
 setup(
     name="HyPyP",
-    version="0.1",
-    author="Florence BRUN, Anaël AYROLLLES, Phoebe CHEN, Amir DJALOVSKI, Suzanne DIKKER, Guillaume DUMAS",
-    author_email="florence.brun@pasteur.fr, anael.ayrollles@pasteur.fr, phoebe.chen1117@gmail.com, amir.djv@gmail.com, suzanne.dikker@nyu.edu, guillaume.dumas@pasteur.fr",
+    version="0.2",
+    author="Florence BRUN, Anaël AYROLLLES, Phoebe CHEN, Amir DJALOVSKI, Yann BEAUXIS, Suzanne DIKKER, Guillaume DUMAS",
+    author_email="florence.brun@pasteur.fr, anael.ayrollles@pasteur.fr, phoebe.chen1117@gmail.com, amir.djv@gmail.com, dev@yannbeauxis.net, suzanne.dikker@nyu.edu, guillaume.dumas@pasteur.fr",
     maintainer="Guillaume DUMAS",
-    maintainer_email="guillaume.dumas@pasteur.fr",
+    maintainer_email="guillaume.dumas@centraliens.net",
     description="The Hyperscanning Python Pipeline.",
     license="BSD",
     keywords="hyperscanning EEG",
     url="https://github.com/GHFC/HyPyP",
     packages=['hypyp'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Topic :: Scientific/Engineering",
         "License :: OSI Approved :: BSD License",
     ],
