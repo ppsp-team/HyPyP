c826ca4d2ade1bd628f647f11bb3186c1e46a216
Fbrun18 <florence.brun@ens-lyon.fr>
2020-06-23 11:19:46 +0200

    adding AR local dico

--- tutorial/getting_started.py
+++ tutorial/getting_started.py
@@ -80,7 +80,7 @@ plt.close('all')
 # rejecting bad epochs, rejecting or interpolating partially bad channels
 # removing the same bad channels and epochs across participants
 # plotting signal before and after (verbose=True)
-cleaned_epochs_AR = prep.AR_local(cleaned_epochs_ICA, verbose=True)
+cleaned_epochs_AR, dic_AR = prep.AR_local(cleaned_epochs_ICA, verbose=True)
 input("Press ENTER to continue")
 plt.close('all')
 
