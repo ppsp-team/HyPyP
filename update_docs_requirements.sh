3b46d6c44f74e3e3683a8fd888e6fd410c8d7f05
Yann Beauxis <dev@yannbeauxis.net>
2020-11-27 11:05:33 +0100

    roll back docs depenencies method

--- update_docs_requirements.sh
+++ update_docs_requirements.sh
@@ -1,4 +1,4 @@
 REQUIREMENTS_PATH=docs/requirements.txt
 
 poetry export -f requirements.txt --dev --without-hashes -o $REQUIREMENTS_PATH
-dephell deps convert
+echo "hypyp==$(poetry run python -c 'import hypyp; print(hypyp.__version__)')" >> $REQUIREMENTS_PATH
