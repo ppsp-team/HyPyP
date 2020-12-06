1b82951dce5c6ae5fc219b329b7fd3c416d1a1a9
Yann Beauxis <pro@yannbeauxis.net>
2020-12-06 11:11:22 +0100

    debug docs requirements

--- hypyp/__init__.py
+++ hypyp/__init__.py
@@ -1,4 +1,5 @@
+import pkg_resources
 from hypyp import analyses, prep, stats, utils, viz
 
-__version__ = "0.2.0-alpha.4"
+__version__ = pkg_resources.get_distribution("hypyp").version
 __all__ = ["analyses", "prep", "stats", "utils", "viz"]
