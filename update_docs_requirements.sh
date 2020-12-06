aeda650b975cecc9b01bb3b12877091c3519a260
Yann Beauxis <pro@yannbeauxis.net>
2020-12-06 20:17:32 +0100

    update_docs_requirements.sh

--- update_docs_requirements.sh
+++ update_docs_requirements.sh
@@ -1,4 +1,8 @@
 REQUIREMENTS_PATH=docs/requirements.txt
 
+echo "upgrade pip"
+poetry run pip install --upgrade pip
+echo "poetry install"
+poetry install
 poetry export -f requirements.txt --dev --without-hashes -o $REQUIREMENTS_PATH
 echo "hypyp==$(poetry run python -c 'import hypyp; print(hypyp.__version__)')" >> $REQUIREMENTS_PATH
