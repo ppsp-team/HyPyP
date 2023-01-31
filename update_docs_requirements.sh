c70b584a50621a6f0f7f9a1984a1183cd2ad380a
Guillaume Dumas <deep@introspection.eu>
2023-01-31 18:25:31 -0500

    Update documentation requirements

--- update_docs_requirements.sh
+++ update_docs_requirements.sh
@@ -4,5 +4,5 @@ echo "upgrade pip"
 poetry run pip install --upgrade pip
 echo "poetry install"
 poetry install
-poetry export -f requirements.txt --dev --without-hashes -o $REQUIREMENTS_PATH
+poetry export -f requirements.txt --with dev --without-hashes -o $REQUIREMENTS_PATH
 echo "hypyp==$(poetry run python -c 'import hypyp; print(hypyp.__version__)')" >> $REQUIREMENTS_PATH
