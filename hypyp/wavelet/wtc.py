split dataframe rows by bin_seconds for correct weighting

hypyp/wavelet/wtc.py
@@ -1,3 +1,4 @@
+from typing import List
 import numpy as np
 import pandas as pd
 
@@ -7,8 +8,10 @@ from .coherence_data_frame import CoherenceDataFrame
 from ..plots import plot_wtc
 from ..utils import downsample_in_time
 
+MASK_THRESHOLD = 0.5
+
 class WTC:
-    def __init__(self, wtc, times, scales, periods, coi, pair: PairSignals):
+    def __init__(self, wtc, times, scales, periods, coi, pair: PairSignals, bin_seconds:float|None=None):
         self.wtc = wtc
 
         self.times = times
@@ -44,6 +47,10 @@ class WTC:
         self.wtc_masked: np.ma.MaskedArray
         self.coherence_metric: float
 
+        self.bin_seconds = bin_seconds
+        self.coherence_metric_bins: List[float] = []
+        self.coherence_masked_bins: List[float] = []
+
         self.compute_coherence_in_coi()
     
     # TODO split in time segments here and make sure we weight our values correctly given time and frequencies
@@ -51,13 +58,36 @@ class WTC:
         mask = self.periods[:, np.newaxis] > self.coi
         self.wtc_masked = np.ma.masked_array(self.wtc, mask)
 
-        # TODO maybe we should weight our average because we have more values at higher frequencies than lower frequencies, due to the coi
         coherence = np.mean(self.wtc_masked)
-        if np.ma.is_masked(coherence):
-            coherence = np.nan
-        elif not np.isfinite(coherence):
+        if np.ma.is_masked(coherence) or not np.isfinite(coherence):
             coherence = np.nan
         self.coherence_metric = coherence
+
+        # Bins
+        self.coherence_metric_bins = []
+        self.coherence_masked_bins = []
+
+        if self.bin_seconds is None:
+            self.coherence_metric_bins = [self.coherence_metric]
+            self.coherence_masked_bins = [np.mean(self.wtc_masked.mask)]
+        else:
+            duration = len(self.times) * self.dt
+            steps = int(duration / self.bin_seconds)
+            size = int(self.bin_seconds / self.dt)
+            for step in range(steps):
+                start = step * size
+                stop = start + size
+                wtc_bin = self.wtc_masked[:, start:stop]
+                coherence_bin = np.mean(wtc_bin)
+                coherence_masked = np.mean(wtc_bin.mask)
+                if np.ma.is_masked(coherence) or not np.isfinite(coherence):
+                    coherence_bin = np.nan
+                    coherence_masked = 1.0
+                elif coherence_masked > MASK_THRESHOLD:
+                    coherence_bin = np.nan
+                self.coherence_metric_bins.append(coherence_bin)
+                self.coherence_masked_bins.append(coherence_masked)
+
     
     def downsample_in_time(self, bins):
         self.times, self.wtc, self.coi, self.coif, _factor = downsample_in_time(self.times, self.wtc, self.coi, self.coif, bins=bins)
@@ -65,26 +95,33 @@ class WTC:
         self.compute_coherence_in_coi()
     
     @property
-    def as_frame_row(self) -> list:
+    def as_frame_rows(self) -> List[List]:
         # IMPORTANT: must match the ordering of COHERENCE_FRAME_COLUMNS
-        return [
-            self.label_dyad,
-            self.is_intra,
-            self.is_shuffle,
-            self.label_s1,
-            self.label_s2,
-            self.label_roi1,
-            self.label_roi2,
-            self.label_ch1,
-            self.label_ch2,
-            self.task,
-            self.epoch,
-            self.section,
-            self.coherence_metric,
-        ]
+        frames = []
+        for bin_id in range(len(self.coherence_metric_bins)):
+            coherence_metric = self.coherence_metric_bins[bin_id]
+            coherence_masked = self.coherence_masked_bins[bin_id]
+            frames.append([
+                self.label_dyad,
+                self.is_intra,
+                self.is_shuffle,
+                self.label_s1,
+                self.label_s2,
+                self.label_roi1,
+                self.label_roi2,
+                self.label_ch1,
+                self.label_ch2,
+                self.task,
+                self.epoch,
+                self.section,
+                bin_id, 
+                coherence_metric,
+                coherence_masked,
+            ])
+        return frames
     
     def to_frame(self) -> CoherenceDataFrame:
-        df = CoherenceDataFrame.from_wtcs([self.as_frame_row])
+        df = CoherenceDataFrame.from_wtcs(self.as_frame_rows)
         return df
 
     #